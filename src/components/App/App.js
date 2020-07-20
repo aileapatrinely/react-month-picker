fc2414f9ab8d3fb13b64aae8cad6f002f44eddd5
Ailea Patrinely <aileapatrinely@gmail.com>
2020-07-20 13:27:15 -0500

    GET sets state

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,16 +3,46 @@ import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  state = {
+    monthList: [
+      {
+        id: 0,
+        name: '',
+      },
+    ],
+  };
+
+  componentDidMount() {
+    this.getCalendar();
+  }
+
+  getCalendar() {
+    axios({
+      method: 'GET',
+      url: '/calendar',
+    })
+      .then((response) => {
+        console.log(response.data);
+        this.setState({
+          monthList: response.data,
+        });
+      })
+      .catch((err) => {
+        console.log('Error:', err);
+        alert('Error in getCalendar');
+      });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Select a Month</h1>
           <h3>SELECTED MONTH GOES HERE</h3>
-          <br/>
+          <br />
         </header>
-        <br/>
-        <p>List of months goes here</p>
+        <br />
+        <p>{this.monthList}</p>
       </div>
     );
   }
